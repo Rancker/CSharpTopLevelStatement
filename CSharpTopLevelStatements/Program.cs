a209b3f1999d98a852eaf8b72d451475dc8f1087
goelchhavi <chhavi.goel@ratesetter.com>
2021-07-22 17:36:32 +0100

    Csharp 9.0 top level statement demo

---
+++ CSharpTopLevelStatements/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Args: " + args?[0]);
+var person = new Person { FirstName = "Chhavi", LastName = "Goel" };
+System.Console.WriteLine($"Name: {person.FirstName} {person.LastName}");
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
